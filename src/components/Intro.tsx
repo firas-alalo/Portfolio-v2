4d15e1da86ff438123cfa63c802fe4548c8aa301
Firas Alalo <Firas.vfx@gmail.com>
2024-02-13 04:38:24 +0100

    Add styled intro texts

---
+++ src/components/Intro.tsx
@@ -0,0 +1,14 @@
+import styles from "../styles/Intro.module.css";
+
+const Intro = () => {
+  return (
+    <div className={styles.sectionOne}>
+      <h1 className={styles.greetOne}>Unveiling My Universe</h1>
+      <h3 className={styles.greetTwo}>
+        Where Passion for Creativity Meets Endless Possibility!
+      </h3>
+    </div>
+  );
+};
+
+export default Intro;
