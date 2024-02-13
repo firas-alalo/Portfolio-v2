f36727c96ecb67cd0fa04c38fc831637fca5024c
Firas Alalo <Firas.vfx@gmail.com>
2024-02-13 04:42:40 +0100

    Render all components on homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
+import { Container, Grid } from "@mui/material";
+import Intro from "./components/Intro";
+import PersonalInfo from "./components/PersonalInfo";
+import TechStack from "./components/TechStack";
+import Contact from "./components/Contact";
+import NavBar from "./components/Navbar";
 import "./App.css";
+import ImageButtons from "./components/Buttons";
 
 function App() {
   return (
     <div className="App">
-      <h1>My New porfolio</h1>
+      <Container>
+        <Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <NavBar />
+            <Intro />
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <PersonalInfo />
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <TechStack />
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <Contact />
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={3}></Grid>
+          <ImageButtons />
+        </Grid>
+      </Container>
     </div>
   );
 }
